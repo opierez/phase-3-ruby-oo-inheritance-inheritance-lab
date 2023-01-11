7e3267fb6809e5d4eb87abfeb1bfb5950a679735
olivia <opierez@gmail.com>
2023-01-10 22:05:41 -0500

    commit tree a2a05ae2069ea80ba7cbf1e65493d35726290165

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require_relative './user'
+
+class Student < User
+
+    attr_reader :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn str
+        knowledge << str
+    end
+
 
 end
